b48f940fbe1e056cbeb5e04d0e5cd75399aa8d5d
Syed Zaid Ali <100484738+syedzaid09@users.noreply.github.com>
2024-02-23 13:00:59 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -4,7 +4,24 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        pass
+        stack=[]
+        mapping={'(':'(','{','[':'['}
+        for char in s:
+            if char in s:
+                if char in mapping.values():
+                    stack.append(char)
+                elif char in mapping.keys():
+                    if not stack or mapping[char] != stack.pop():
+                        return False
+                else:
+                    return False
+        return not stack
+s=Solution()
+print(s.is_valid("()"))
+print(s.is_valid("()[]{}"))
+print(s.is_valid("(]"))
+
+        
     
 
 
